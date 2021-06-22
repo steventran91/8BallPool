a7c271a440d5f8b730726525a4274ffd1baf39a5
StevenTran91 <sdtran91@gmail.com>
2021-06-22 16:59:41 -0700

    add power/movement to cue stick

--- stick.js
+++ stick.js
@@ -1,21 +1,24 @@
 const STICK_ORIGIN = new Vector2(970, 11);
 
-function Stick(position){
+function Stick(position, onShoot){
     this.position = position;
     this.rotation = 0;
+    this.origin = STICK_ORIGIN.copy();
+    this.power = 0;
+    this.onShoot = onShoot;
 }
 
 Stick.prototype.update = function(){
-    // this.position = Mouse.position;
-
-    // if (Mouse.left.pressed) {
-    //     console.log("pressed left");
-    // }
+    if (Mouse.left.down) {
+        this.increasePower();
+    } else if (this.power > 0) {
+        this.shoot();
+    }
     this.updateRotation();
 }
 
 Stick.prototype.draw = function(){
-    Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN, this.rotation);
+    Canvas.drawImage(sprites.stick, this.position, this.origin, this.rotation);
 }
 
 Stick.prototype.updateRotation = function(){
@@ -23,4 +26,14 @@ Stick.prototype.updateRotation = function(){
     let adjacent = Mouse.position.x - this.position.x;
 
     this.rotation = Math.atan2(opposite, adjacent);
+}
+
+Stick.prototype.increasePower = function(){
+    this.power += 100;
+    this.origin.x += 5;
+}
+
+Stick.prototype.shoot = function() {
+    this.onShoot(this.power, this.rotation);
+    this.power = 0;
 }
